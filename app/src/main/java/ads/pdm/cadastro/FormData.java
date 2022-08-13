13d9af599378764a18d6aa1c671e8b0c30752a4e
kpi-jps <jps.spj@gmail.com>
2022-08-12 23:46:47 -0300

    under construction

---
+++ app/src/main/java/ads/pdm/cadastro/FormData.java
@@ -0,0 +1,95 @@
+package ads.pdm.cadastro;
+
+import android.text.Editable;
+
+public class FormData {
+    private String name;
+    private String phone;
+    private String email;
+    private boolean choiceEmailList;
+    private String gender;
+    private String city;
+    private String state;
+
+    public FormData(String name, String phone,
+                    String email, String s, boolean choiceEmailList,
+                    String gender, String city,
+                    String state) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.choiceEmailList = choiceEmailList;
+        this.gender = gender;
+        this.city = city;
+        this.state = state;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isChoiceEmailList() {
+        return choiceEmailList;
+    }
+
+    public void setChoiceEmailList(boolean choiceEmailList) {
+        this.choiceEmailList = choiceEmailList;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        String inEmailList = (choiceEmailList) ? "Sim" : "Não";
+        return "Dados formulários : \n" +
+                "Nome: " + name + "\n" +
+                "Telefone: " + phone + "\n" +
+                "Email: " + email + "\n" +
+                "Incluído na lista de email : " + inEmailList + "\n" +
+                "Sexo: " + gender + "\n" +
+                "Cidade: " + city + "\n" +
+                "Estado: " + state + "\n";
+    }
+}
--- app/src/main/java/ads/pdm/cadastro/MainActivity.java
+++ app/src/main/java/ads/pdm/cadastro/MainActivity.java
@@ -3,11 +3,37 @@ package ads.pdm.cadastro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import ads.pdm.cadastro.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding amb;
+
+    private FormData getFormData() {
+        String gender = (amb.rbFemale.isChecked()) ? "Feminino" : "Masculino";
+        return new FormData(
+                amb.etName.getText().toString(),
+                amb.etName.getText().toString(),
+                amb.etPhone.getText().toString(),
+                amb.etEmail.getText().toString(),
+                amb.cbEmail.isChecked(),
+                gender,
+                amb.etCity.getText().toString(),
+                amb.spState.getSelectedItem().toString()
+        );
+    }
+
+    private void clearForm() {
+        amb.etName.getText().clear();
+        amb.etPhone.getText().clear();
+        amb.etEmail.getText().clear();
+        amb.cbEmail.setChecked(false);
+        amb.rbFemale.setChecked(false);
+        amb.rbMale.setChecked(false);
+        amb.etCity.getText().clear();
+        amb.spState.setSelection(0);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +41,18 @@ public class MainActivity extends AppCompatActivity {
         amb = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(amb.getRoot());
 
+        amb.btSave.setOnClickListener(view -> {
+            FormData formData = getFormData();
+            Toast.makeText(getApplicationContext(),
+                    formData.toString(),
+                    Toast.LENGTH_LONG)
+                    .show();
+            clearForm();
+        });
+
+
+        amb.btClear.setOnClickListener(view -> {
+            clearForm();
+        });
     }
 }
